303b5b56b317e669bd9f4f2738f44be1a23acfe0
Haidara <sidymohamedcherifhaidara02@gmail.com>
2025-03-06 13:36:27 -0800

    change

--- database/factories/professeurFactory.php
+++ database/factories/professeurFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\professeur>
  */
-class professeurFactory extends Factory
+class ProfesseurFactory extends Factory
 {
     /**
      * Define the model's default state.
